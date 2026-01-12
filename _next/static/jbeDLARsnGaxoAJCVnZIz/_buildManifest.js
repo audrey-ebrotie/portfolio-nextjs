511a9be656fbc7bd0d21059dcfa9959ac76cdfa4
Audrey EBROTIE <audrey.ebrotie@gmail.com>
2026-01-12 10:54:04 +0100

    Updates

--- _next/static/jbeDLARsnGaxoAJCVnZIz/_buildManifest.js
+++ _next/static/jbeDLARsnGaxoAJCVnZIz/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio-nextjs//_next/:path+",
+        "destination": "/portfolio-nextjs/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
